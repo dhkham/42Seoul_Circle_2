74540f9aed6d4dd01b6908086317a8a3a4026ae7
dhkham <khamdong@gmail.com>
2023-03-26 19:13:38 +0900

    modified run_cmd

--- pipex/run_cmd.c
+++ pipex/run_cmd.c
@@ -6,28 +6,18 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 15:34:50 by dkham             #+#    #+#             */
-/*   Updated: 2023/03/26 17:26:26 by dkham            ###   ########.fr       */
+/*   Updated: 2023/03/26 19:00:52 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-// info->paths, info->cmds 값 이용해 커맨드 실행
-// path, cmd 값 이어 붙여서 access() 로 확인
-// access() 성공하면 fork 후 자식 프로세스에서 execve() 실행
-
 void	run_cmd(int argc, char **argv, char **envp, t_info *info)
 {
 	int		i;
-	char	*path;
-	char	*cmd;
-	char	**cmd_split;
-	char	*tmp;
 	pid_t	pid;
 	int		pipe_fd[2];
 
-	//각 cmd에 대해 path의 모든 값들과 cmd 붙여서 access()로 확인
-	//access() 성공하면 fork() 후 자식 프로세스에서 execve() 실행
 	i = 0;
 	while (i < info->num_cmd)
 	{
@@ -40,8 +30,9 @@ void	run_cmd(int argc, char **argv, char **envp, t_info *info)
 			child_proc(info, i, pipe_fd, envp); // 처음, 끝, 중간 커맨드 나눠서 구현
 		else //부모 프로세스 실행
 			parent_proc(info, i, pipe_fd);
+		i++;
 	}
-	while (i < info->num_cmd)
+	while (i-- > 0)
 		wait(NULL); // 수거
 }
 
@@ -51,56 +42,83 @@ void	child_proc(t_info *info, int i, int *pipe_fd, char **envp)
 	char	*path;
 	char	*cmd;
 	char	**cmd_split;
-	char	*tmp;
 
-	j = 0;// access 검사
+	j = 0;
 	while (info->paths[j])
 	{
 		path = ft_strjoin(info->paths[j], "/");
-		cmd_split = ft_split(info->cmds[i], ' '); // cmd를 split
+		cmd_split = ft_split(info->cmds[i], ' ');
 		cmd = ft_strjoin(path, cmd_split[0]);
 		free(path);
-		if (access(cmd, F_OK) == 0) // cmd가 존재하면
+		if (access(cmd, F_OK) == 0)
 			break ;
 		j++;
 	}
 	if (info->paths[j] == NULL) // cmd가 존재하지 않으면
 		exit(1); // error 처리 추가
-	if (i == 0) // 처음 커맨드
+	handle_fd(info, pipe_fd, i);
+	if (execve(cmd, cmd_split, envp) == -1)
+		exit(1); //free(cmd_split[0]); 보류
+}
+
+void	handle_fd(t_info *info, int *pipe_fd, int i)
+{
+	if (i == 0)
 	{
 		close(pipe_fd[0]); // child에서 fd[0] 닫기 (infile에서 읽어오기 때문)
 		if (dup2(info->input_fd, 0) == -1) // infile에서 읽어온다
 			exit(1); // error 처리 추가
-		if (dup2(pipe_fd[1], 1) == -1) // 출력 시 pipe_fd[1]로 출력
+		if (dup2(pipe_fd[1], 1) == -1) // 출력 시 pipe_fd[1]로 출력 (필요?)
 			exit(1); // error 처리 추가
-		close(pipe_fd[1]); // child에서 fd[1] 닫기 (outfile으로 출력하기 때문
-		close(info->input_fd); // child에서 fd[0] 닫기 (infile에서 읽어오기 때문
+		close(pipe_fd[1]); // child에서 fd[1] 닫기
+		close(info->input_fd); // child에서 fd[0] 닫기
 	}
-	else if (i == info->num_cmd - 1) // 마지막 커맨드
+	else if (i == info->num_cmd - 1)
 	{
+		close(pipe_fd[0]); //필요?
 		close(pipe_fd[1]); // child에서 fd[1] 닫기 (outfile으로 출력하기 때문)
 		if (dup2(info->output_fd, 1) == -1) // outfile으로 출력
 			exit(1); // error 처리 추가
+		close(info->output_fd); // 필요?
 	}
-	else // 중간 커맨드
+	else
 	{
-		close(pipe_fd[0]);
+		close(pipe_fd[0]); // parent에서 dup2(pipe_fd[0], 0); 해준 후 포크 해줘서 필요 없는듯
 		if (dup2(pipe_fd[1], 1) == -1) // 출력 시 pipe_fd[1]로 출력
 			exit(1); // error 처리 추가
+		close(pipe_fd[1]); // 필요?
 	}
-	if (execve(cmd, cmd_split, envp) == -1) // execve() 실행
-		exit(1); //free(cmd_split[0]); // 보류
 }
 
 void	parent_proc(t_info *info, int i, int *pipe_fd)
 {
-	int		status;
-
-	// 인덱스 별로 시작, 끝, 중간에 대해 fd 처리
-	// 처음 커맨드는 pipe_fd[1]을 close
-	// 끝 커맨드는 pipe_fd[0]을 close
-	// 중간 커맨드는 pipe_fd[0], pipe_fd[1]을 close
 	close(pipe_fd[1]);
-	dup2(pipe_fd[0], 0);
-	close(pipe_fd[0]); // close 후 dup2로 fd 처리하면 에러
+	dup2(pipe_fd[0], 0); // 다음 포크 시 자식이 파이프에서 읽어올 수 있도록 pipe_fd[0]을 input으로 설정
+	close(pipe_fd[0]); // close 후 dup2로 fd 처리하면 에러 (dup2 먼저)
 }
+
+//void	first_cmd(t_info *info, int *pipe_fd)
+// {
+// 		close(pipe_fd[0]); // child에서 fd[0] 닫기 (infile에서 읽어오기 때문)
+// 		if (dup2(info->input_fd, 0) == -1) // infile에서 읽어온다
+// 			exit(1); // error 처리 추가
+// 		if (dup2(pipe_fd[1], 1) == -1) // 출력 시 pipe_fd[1]로 출력 (필요?)
+// 			exit(1); // error 처리 추가
+// 		close(pipe_fd[1]); // child에서 fd[1] 닫기
+// 		close(info->input_fd); // child에서 fd[0] 닫기
+// }
+
+// void	last_cmd(t_info *info, int *pipe_fd)
+// {
+// 		// close(pipe_fd[0]); 필요?
+// 		close(pipe_fd[1]); // child에서 fd[1] 닫기 (outfile으로 출력하기 때문)
+// 		if (dup2(info->output_fd, 1) == -1) // outfile으로 출력
+// 			exit(1); // error 처리 추가
+// }
+
+// void	mid_cmd(t_info *info, int *pipe_fd)
+// {
+// 	close(pipe_fd[0]); // parent에서 dup2(pipe_fd[0], 0); 해준 후 포크 해줘서 필요 없는듯
+// 	if (dup2(pipe_fd[1], 1) == -1) // 출력 시 pipe_fd[1]로 출력
+// 		exit(1); // error 처리 추가
+// }
